6a9478ffed358f9545e39e43e788185a9c124dd0
peter hausel <peter_kovac11@hotmail.com>
2012-02-08 14:25:42 -0500

    upgrade repo location

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,9 @@ object MinimalBuild extends Build {
   lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
     version := "2.0-RC1-SNAPSHOT",
     organization := "com.typesafe",
-    resolvers += Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns),
+    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
     libraryDependencies += "play" %% "play" % "2.0-RC1-SNAPSHOT",
     mainClass in (Compile, run) := Some("play.core.server.NettyServer")
   )
   
-} 
+ 
