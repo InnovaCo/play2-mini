434ba60f8eece0727b60971fd2b0b6c56cf2f650
peter hausel <peter_kovac11@hotmail.com>
2012-02-10 08:13:44 -0500

    making sure we pick up the correct typesafe repo for play

--- project/Build.scala
+++ project/Build.scala
@@ -3,12 +3,14 @@ import Keys._
 
 object MinimalBuild extends Build {
   
+  lazy val buildVersion =  "2.0-RC1-SNAPSHOT"
+  lazy val typesafeVersion = if (buildVersion.endsWith("SNAPSHOT")) "snapshots" else "releases"
+  
   lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
-    version := "2.0-RC1-SNAPSHOT",
+    version := buildVersion,
     organization := "com.typesafe",
-    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
-    libraryDependencies += "play" %% "play" % "2.0-RC1-SNAPSHOT",
+    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/"+typesafeVersion,
+    libraryDependencies += "play" %% "play" % buildVersion,
     mainClass in (Compile, run) := Some("play.core.server.NettyServer")
   )
-  
- 
+}
