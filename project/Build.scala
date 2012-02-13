f84cedac0bc7ac1fbac061e4e7fb7c53668e960b
peter hausel <peter_kovac11@hotmail.com>
2012-02-13 17:22:42 -0500

    fixes samples' sbt setup

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,6 @@ object MinimalBuild extends Build {
   
   lazy val buildVersion =  "2.0-RC1-SNAPSHOT"
   
-  lazy val typesafeVersion = if (buildVersion.endsWith("SNAPSHOT")) "snapshots" else "releases"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/ivy-snapshots/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
--- samples/scala/project/Build.scala
+++ samples/scala/project/Build.scala
@@ -5,17 +5,15 @@ object MinimalBuild extends Build {
   
   lazy val buildVersion =  "2.0-RC1-SNAPSHOT"
   
-  lazy val typesafeVersion = if (buildVersion.endsWith("SNAPSHOT")) "snapshots" else "releases"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/ivy-snapshots/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
 
-  lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
+  lazy val root = Project(id = "play-mini-scala-sample", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
-    publishTo := Some(repo),
-    organization := "com.typesafe",
+    organization := "my.org",
     resolvers += repo,
-    libraryDependencies += "play" %% "play" % buildVersion,
+    libraryDependencies += "com.typesafe" %% "play-mini" % buildVersion,
     mainClass in (Compile, run) := Some("play.core.server.NettyServer")
   )
 }
