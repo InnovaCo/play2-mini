6eea19d7fa2a989b80ea20a1a194f660628ae4f7
peter hausel <peter_kovac11@hotmail.com>
2012-02-12 20:45:41 -0500

    start publishing play mini RC

--- samples/scala/project/Build.scala
+++ samples/scala/project/Build.scala
@@ -3,10 +3,19 @@ import Keys._
 
 object MinimalBuild extends Build {
   
+  lazy val buildVersion =  "2.0-RC1-SNAPSHOT"
+  
+  lazy val typesafeVersion = if (buildVersion.endsWith("SNAPSHOT")) "snapshots" else "releases"
+  lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/ivy-snapshots/"
+  lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
+
   lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
-    resolvers += Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns),
-    libraryDependencies += "com.typesafe" %% "play-mini" % "2.0-RC1-SNAPSHOT",
+    version := buildVersion,
+    publishTo := Some(repo),
+    organization := "com.typesafe",
+    resolvers += repo,
+    libraryDependencies += "play" %% "play" % buildVersion,
     mainClass in (Compile, run) := Some("play.core.server.NettyServer")
   )
-  
-} 
+}
