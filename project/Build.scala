20fcdf492db2740dc920863607ebe6c802a4e124
peter hausel <peter_kovac11@hotmail.com>
2012-02-16 09:44:25 -0500

    fix snapshot publishing

--- project/Build.scala
+++ project/Build.scala
@@ -5,19 +5,19 @@ object MinimalBuild extends Build {
   
   lazy val buildVersion =  "2.0-RC3-SNAPSHOT"
   
-  lazy val typesafePub = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/ivy-releases/"
-  lazy val typesafeSnapshotPub = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/ivy-snapshots/"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
-  lazy val repoPublish = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshotPub else typesafePub  
   
-
   lazy val play =  "play" %% "play" % buildVersion 
 
   lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
-    publishTo := Some(repoPublish),
+    publishTo <<= (version) { version: String =>
+                val nexus = "http://repo.typesafe.com/typesafe/"
+                if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "ivy-snapshots/") 
+                else                                   Some("releases"  at nexus + "ivy-releases/")
+    },
     organization := "com.typesafe",
     resolvers += repo,
     libraryDependencies += play,
